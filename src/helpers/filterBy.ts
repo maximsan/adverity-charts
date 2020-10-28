1b792c9f5b3ca73995ca1a65e0367ce096a3584f
maximsan <maximuzz@yandex.ru>
2020-10-28 19:27:21 +0300

    refactoring: rewrite filter helpers to more declarative view

--- src/helpers/filterBy.ts
+++ src/helpers/filterBy.ts
@@ -1,33 +1,25 @@
-import { FilterKey, FilterType } from '../shared/types';
-import { AdData } from '../types';
-import { ALL } from '../shared/constants';
+import {FilterKey, FilterType} from '../shared/types';
+import {AdData} from '../types';
+import {ALL} from '../shared/constants';
 
 export const filterBy = (initialData: AdData[], filters: FilterType) => {
-  let filteredData = [...initialData];
-
-  for (let key of Object.keys(filters)) {
-    if (key === FilterKey.DATA_SOURCE) {
-      filteredData = filterByDataSources(filteredData, filters[key]);
-    } else if (key === FilterKey.CAMPAIGN) {
-      filteredData = filterByCampaigns(filteredData, filters[key]);
+    if (initialData.length) {
+        Object.keys(filters).forEach((key) => {
+            if (key === FilterKey.DATA_SOURCE) {
+                initialData = filterByField(initialData, FilterKey.DATA_SOURCE, filters[key]);
+            } else if (key === FilterKey.CAMPAIGN) {
+                initialData = filterByField(initialData, FilterKey.CAMPAIGN, filters[key]);
+            }
+        })
     }
-  }
 
-  return filteredData;
+    return initialData;
 };
 
-const filterByCampaigns = (data: AdData[], campaignFilters: string[]): AdData[] => {
-  if (campaignFilters[0] !== ALL) {
-    return data.filter(({ campaign }) => campaignFilters.indexOf(campaign) !== -1);
-  }
+const filterByField = (data: AdData[], field: string, filters: string[]): AdData[] => {
+    if (filters[0] !== ALL) {
+        return data.filter((item) => filters.indexOf((item as any)[field]) !== -1);
+    }
 
-  return data;
-};
-
-const filterByDataSources = (data: AdData[], dataSourceFilters: string[]): AdData[] => {
-  if (dataSourceFilters[0] !== ALL) {
-    return data.filter(({ dataSource }) => dataSourceFilters.indexOf(dataSource) !== -1);
-  }
-
-  return data;
-};
+    return data;
+}
